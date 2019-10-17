4f90323ef0070c4bba401d9cdfef769ea3fee458
Nuniemsis <nunocarvalhodossantos@gmail.com>
2019-10-17 18:50:44 +0200

    c

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,15 @@ import SearchBar from './SearchBar.js';
 import NavBar from './NavBar.js';
 
 
-
-const parentDivStyle = {
-  "width": "100%",
-   "display": "table",
-   "margin-right": "50px"
-}
-
-const ppDivStyle = {
-  "display": "table-row"
-}
-
-
 const lDivStyle = {
-  "width": "300px",
-  "height": "300px",
+  "width": "50%",
+  "height": "inherit",
    "display": "table-cell"
 }
 
 const rDivStyle = {
-  "height": "300px",
-  "width": "300px",
+    "width": "50%",
+    "height": "100%",
    "display": "table-cell"
 }
 
@@ -42,10 +30,10 @@ class App extends Component {
       <NavBar/>
 
 
-      <div style={parentDivStyle}>
-          <div style={ppDivStyle}>
-              <div style={rDivStyle}> <SearchBar/> </div>
-              <div style={lDivStyle}> <GoogleMapContainer/></div>
+      <div>
+          <div className="parent-div">
+            <div style={lDivStyle}> <SearchBar/></div>
+              <div style={rDivStyle}> <GoogleMapContainer/></div>
 
           </div>
       </div>
