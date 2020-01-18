fdf8e7ca88ece2f617cd01d3dc908fee66c8d3dd
Nuniemsis <nunocarvalhodossantos@gmail.com>
2020-01-18 17:30:25 +0100

    add cidr

--- src/components/Cidr.js
+++ src/components/Cidr.js
@@ -94,6 +94,8 @@ class Cidr extends Component {
                                 }
                             <form onSubmit={this.handleSubmit}>
                                 <div style={{marginTop: "5%"}}>
+                                    <h3>Calculate first and last IP of CIDR block</h3>
+                                        <p3>example: 10.12.12.12/27</p3>
                                     <div className="input-group mb-3">
 
 
@@ -107,6 +109,20 @@ class Cidr extends Component {
                                             </button>
                                         </div>
                                     </div>
+
+
+                                    Classless Inter-Domain Routing (CIDR /ˈsaɪdər, ˈsɪ-/) is a method for allocating IP addresses and IP routing. The Internet Engineering Task Force introduced CIDR in 1993 to replace the previous addressing architecture of classful network design in the Internet. Its goal was to slow the growth of routing tables on routers across the Internet, and to help slow the rapid exhaustion of IPv4 addresses.[1][2]
+
+IP addresses are described as consisting of two groups of bits in the address: the most significant bits are the network prefix, which identifies a whole network or subnet, and the least significant set forms the host identifier, which specifies a particular interface of a host on that network. This division is used as the basis of traffic routing between IP networks and for address allocation policies.
+
+Whereas classful network design for IPv4 sized the network prefix as one or more 8-bit groups, resulting in the blocks of Class A, B, or C addresses, Classless Inter-Domain Routing allocates address space to Internet service providers and end users on any address-bit boundary. In IPv6, however, the interface identifier has a fixed size of 64 bits by convention, and smaller subnets are never allocated to end users.
+
+CIDR encompasses several concepts. It is based on the variable-length subnet masking (VLSM) technique, which allows the specification of arbitrary-length prefixes. CIDR introduced a new method of representation for IP addresses, now commonly known as CIDR notation, in which an address or routing prefix is written with a suffix indicating the number of bits of the prefix, such as 192.0.2.0/24 for IPv4, and 2001:db8::/32 for IPv6. CIDR introduced an administrative process of allocating address blocks to organizations based on their actual and short-term projected needs. The aggregation of multiple contiguous prefixes resulted in supernets in the larger Internet, which whenever possible are advertised as aggregates, thus reducing the number of entries in the global routing table.
+
+                                    <a href="https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing">Sources(Wikipedia)</a>
+
+
+
                                 </div>
                             </form>
                         </div>
