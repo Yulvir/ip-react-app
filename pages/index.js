04a661bf634a654396c7f8f4f167b45f00ae4653
Nuniemsis <nunocarvalhodossantos@gmail.com>
2020-02-18 22:34:27 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,43 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import App from '../components/app'
-import {fetchMyIp, fetchMyIpAndGetInfoIp, setMyIpAndGetInfoIp} from "../actions/actions-creators";
+import {fetchMyIp, fetchMyIpAndGetInfoIp, getInfoIp, setMyIpAndGetInfoIp} from "../actions/actions-creators";
+import {setMyIp} from "../actions/actions";
+
+const requestIp = require('request-ip');
+
 
 class Index extends React.Component {
-  static  async getInitialProps({ reduxStore, req }) {
-    const isServer = !!req;
+    static async getInitialProps({reduxStore, req}) {
+        const isServer = !!req;
 
-    // const host = req.headers["host"];
-    // console.log(host);
-    // if(host.includes("localhost")){
-    //   await  reduxStore.dispatch(fetchMyIpAndGetInfoIp())
-    // } else {
-    //     await reduxStore.dispatch(setMyIpAndGetInfoIp(host));
-    // }
+        if (typeof req != "undefined") {
+            const myIp = requestIp.getClientIp(req);
+            console.log(myIp);
 
-    await  reduxStore.dispatch(fetchMyIpAndGetInfoIp());
-    return {}
-  }
+            if (["::1", "::ffff:127.0.0.1", "127.0.0.1"].includes(myIp)) {
+                console.log("Browser at localhost");
+                await reduxStore.dispatch(fetchMyIpAndGetInfoIp())
+
+            } else {
+                console.log("Browser at " + myIp);
+                reduxStore.dispatch(setMyIp(myIp));
+                await reduxStore.dispatch(getInfoIp(myIp));
+            }
+        }
 
 
-  render() {
-    return (
-        <div>
-    <App/>
-        </div>
-    )
-  }
+        return {}
+    }
+
+
+    render() {
+        return (
+            <div>
+                <App/>
+            </div>
+        )
+    }
 }
 
 export default connect()(Index)
